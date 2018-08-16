6e4409c65f0329d80a41c72e42f1621dad13b566
riyapahuja1994 <riyapahuja1994@gmail.com>
2018-08-16 18:23:13 +0530

    Changed post & put route url from product to products to match other routes.

--- app/routes/inventory_routes.js
+++ app/routes/inventory_routes.js
@@ -1,74 +1,72 @@
-var ObjectId = require('mongodb').ObjectID;
+var ObjectId = require("mongodb").ObjectID;
 
-module.exports = function(app, db){
-
-    app.get('/inventory_products', (req,res) => {
-        db.collection('product_inventory').find({}).toArray((err,result) => {
-            if(err)
-            {
-                
-                res.send({'error':'An error occurred while fetching product inventory'});
-            }
-            else
-            {
-                res.send(result);
-            }
-        })
-    })
-
-    app.get('/inventory_products/:product_id', (req,res) => {
-        console.log(req.params.product_id);
-        const id = req.params.product_id ;
-        const queryParams = { 'product_id' : (id) };
-        db.collection('product_inventory').findOne(queryParams, (err,result) => {
-            if(err)
-            {
-                res.send({'error':'An error occurred while fetching product inventory'});
-            }
-            else
-            {
-                res.send(result);
-            }
-        })
-    })
-
-    app.post('/inventory_product', (req, res) => {
-        let product = req.body;
-        db.collection('product_inventory').insert(product, (err,result) => {
-            if (err) { 
-                res.send({ 'error': 'An error occurred while insert new product' }); 
-            } 
-            else {
-                res.send(result.ops[0]);
-            }
-        })
-    })
-
-    app.put('/inventory_product/:product_id', (req, res) => {
-        const id = req.params.product_id;
-        const queryParams = { 'product_id' : (id) };
-        let product = req.body;
-        db.collection('product_inventory').update(queryParams, product, (err, result) => {
-          if (err) { 
-            res.send({ 'error': 'An error occurred while updating the product' }); 
-          } else {
-            res.send(product);
-          }
-        });
+module.exports = function(app, db) {
+  app.get("/inventory_products", (req, res) => {
+    db.collection("product_inventory")
+      .find({})
+      .toArray((err, result) => {
+        if (err) {
+          res.send({
+            error: "An error occurred while fetching product inventory"
+          });
+        } else {
+          res.send(result);
+        }
       });
+  });
 
-      app.delete('/inventory_product/:product_id', (req,res)=>{
-        const id = req.params.product_id;
-        const queryParams = { 'product_id' : (id) };
-        db.collection('product_inventory').remove(queryParams, (err,item)=>{
-          if(err)
-          { 
-            res.send({'error': 'An error occurred while deleting the product'});
-          }
-          else
-          {
-            res.send('Product ' + id + ' deleted!');
-          }
+  app.get("/inventory_products/:product_id", (req, res) => {
+    console.log(req.params.product_id);
+    const id = req.params.product_id;
+    const queryParams = { product_id: id };
+    db.collection("product_inventory").findOne(queryParams, (err, result) => {
+      if (err) {
+        res.send({
+          error: "An error occurred while fetching product inventory"
         });
-      });
-}
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+  app.post("/inventory_products", (req, res) => {
+    let product = req.body;
+    db.collection("product_inventory").insert(product, (err, result) => {
+      if (err) {
+        res.send({ error: "An error occurred while insert new product" });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.put("/inventory_products/:product_id", (req, res) => {
+    const id = req.params.product_id;
+    const queryParams = { product_id: id };
+    let product = req.body;
+    db.collection("product_inventory").update(
+      queryParams,
+      product,
+      (err, result) => {
+        if (err) {
+          res.send({ error: "An error occurred while updating the product" });
+        } else {
+          res.send(product);
+        }
+      }
+    );
+  });
+
+  app.delete("/inventory_products/:product_id", (req, res) => {
+    const id = req.params.product_id;
+    const queryParams = { product_id: id };
+    db.collection("product_inventory").remove(queryParams, (err, item) => {
+      if (err) {
+        res.send({ error: "An error occurred while deleting the product" });
+      } else {
+        res.send("Product " + id + " deleted!");
+      }
+    });
+  });
+};
--- app/routes/shopping_routes.js
+++ app/routes/shopping_routes.js
@@ -1,61 +1,82 @@
-var ObjectId = require('mongodb').ObjectID;
+var ObjectId = require("mongodb").ObjectID;
 
-module.exports = function(app, db){
-
-    app.get('/products', (req,res) => {
-        db.collection('product_inventory').find({},{projection: { _id: 0, product_id: 1, product_name: 1, product_image: 1, product_price: 1, product_discount:1 }}).toArray(function(err,result) {
-            if(err)
-            {
-                res.send({'error':'An error occurred while fetching products'});
-            }
-            else
-            {
-                console.log(result);
-                res.send(result);
-            }   
-        })
-    })
-
-    app.get('/products/:product_id', (req,res) => {
-        const id = req.params.product_id ;
-        const queryParams = { 'product_id': id };
-        db.collection('product_inventory').findOne(queryParams,{projection: {_id: 0, product_id: 1, product_name: 1, product_image: 1, product_category: 1, product_details: 1, product_price: 1, product_discount: 1  }},(err,result) => {
-            if(err)
-            {
-                res.send({'error':'An error occurred while fetching product inventory'});
-            }
-            else
-            {
-                res.send(result);
-            }
-        })
-    })
-
-    app.post('/product', (req, res) => {
-        let product = req.body;
-        db.collection('product_cart').insert(product, (err,result) => {
-            if (err) { 
-                res.send({ 'error': 'An error occurred while inserting new product' }); 
-            } 
-            else {
-                res.send(result.ops[0]);
-            }
-        })
-    })
-
-    app.delete('/product/:product_id', (req,res)=>{
-        const id = req.params.product_id ;
-        const queryParams = { 'product_id': id };
-        db.collection('product_cart').remove(queryParams, (err,item)=>{
-          if(err)
-          { 
-            res.send({'error': 'An error occurred while deleting product from your cart.'});
+module.exports = function(app, db) {
+  app.get("/products", (req, res) => {
+    db.collection("product_inventory")
+      .find(
+        {},
+        {
+          projection: {
+            _id: 0,
+            product_id: 1,
+            product_name: 1,
+            product_image: 1,
+            product_price: 1,
+            product_discount: 1
           }
-          else
-          {
-            res.send('Product deleted from your cart!');
-          }
-        });
+        }
+      )
+      .toArray(function(err, result) {
+        if (err) {
+          res.send({ error: "An error occurred while fetching products" });
+        } else {
+          console.log(result);
+          res.send(result);
+        }
       });
-    
-}
+  });
+
+  app.get("/products/:product_id", (req, res) => {
+    const id = req.params.product_id;
+    const queryParams = { product_id: id };
+    db.collection("product_inventory").findOne(
+      queryParams,
+      {
+        projection: {
+          _id: 0,
+          product_id: 1,
+          product_name: 1,
+          product_image: 1,
+          product_category: 1,
+          product_details: 1,
+          product_price: 1,
+          product_discount: 1
+        }
+      },
+      (err, result) => {
+        if (err) {
+          res.send({
+            error: "An error occurred while fetching product inventory"
+          });
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+
+  app.post("/products", (req, res) => {
+    let product = req.body;
+    db.collection("product_cart").insert(product, (err, result) => {
+      if (err) {
+        res.send({ error: "An error occurred while inserting new product" });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.delete("/products/:product_id", (req, res) => {
+    const id = req.params.product_id;
+    const queryParams = { product_id: id };
+    db.collection("product_cart").remove(queryParams, (err, item) => {
+      if (err) {
+        res.send({
+          error: "An error occurred while deleting product from your cart."
+        });
+      } else {
+        res.send("Product deleted from your cart!");
+      }
+    });
+  });
+};
